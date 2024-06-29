cdcf2b71d78956a4f06c0aee6e7c9c14f7e4f24a
Udemezue Iloabachie <udemezue@gmail.com>
2024-06-29 08:48:02 -0400

    container div added

--- app.py
+++ app.py
@@ -253,7 +253,7 @@ def logout():
 
 
 if __name__ == "__main__":
-    # app.run(host='0.0.0.0', port=5000, debug=True) # for dev testing
-    app.run() # For production
+    app.run(host='0.0.0.0', port=5000, debug=True) # for dev testing
+    # app.run() # For production
     
     
